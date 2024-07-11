b1e9ad81ae2c6da4682d9253c4e19bd8a1f45b54
UR-deR <shota.furuno@claves.co.jp>
2024-07-12 00:09:52 +0900

    receive post request and respond with request param

--- app.ts
+++ app.ts
@@ -2,8 +2,16 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.post('/', (req, res) => {
+  const name = req.body.name
+  
+  res.send(`Hello ${name}!`)
 })
 
 app.listen(port, () => {
