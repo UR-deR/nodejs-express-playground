8092ef8e294563299964acfe6af783ef798b7545
UR-deR <shota.furuno@claves.co.jp>
2024-07-12 00:27:09 +0900

    chore: Update app.ts to handle form-urlencoded requests

--- app.ts
+++ app.ts
@@ -1,8 +1,12 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 
 app.use(express.json());
+app.use(bodyParser.json()); 
+app.use(express.urlencoded({ extended: true }));
+
 
 app.get('/', (req, res) => {
   res.send({
@@ -16,6 +20,17 @@ app.post('/', (req, res) => {
     return res.status(400).send('Bad Request');
   }
 
+  res.status(201).send(req.body);
+});
+
+app.post("/form-urlencoded", (req, res) => {
+  const contentType = req.headers['content-type'];
+  if (contentType !== 'application/x-www-form-urlencoded') {
+    return res.status(400).send('Bad Request');
+  }
+  console.log(req.body);
+  
+  
   res.status(201).send(req.body);
 });
 
