026be076a1a081fe1c2c3ceff2e7b6ad9444a649
UR-deR <shota.furuno@claves.co.jp>
2024-07-12 00:12:00 +0900

    format code

--- app.ts
+++ app.ts
@@ -1,19 +1,23 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
-app.use(express.json())
+app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
 app.post('/', (req, res) => {
-  const name = req.body.name
+  const contentType = req.headers['content-type'];
+  if (contentType !== 'application/json') {
+    return res.status(400).send('Bad Request');
+  }
+  const name = req.body.name;
 
-  res.status(201).send(`Hello ${name}!`)
-})
+  res.status(201).send(`Hello ${name}!`);
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
